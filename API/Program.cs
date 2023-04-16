cb45c60d342d2a9753c7dee16ec5f03fca908cf8
Lionel Ngobesing <ngobesinglionel@gmail.com>
2023-04-16 22:50:00 +0300

    Second Commit. Implementing the React front UI

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddDbContext<StoreContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +24,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(opt => {
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+});
 
 app.UseAuthorization();
 
---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,39 @@
+import Catalog from "../../features/catalog/catalog";
+import { Container, CssBaseline, createTheme } from "@mui/material";
+import Header from "./header";
+import { ThemeProvider } from "@emotion/react";
+import { useState } from "react";
+
+
+
+function App() {
+
+const [darkMode, setDDarkMode] = useState(false);
+const paletteType = darkMode ? 'dark' : 'light';
+
+const theme = createTheme({
+  palette: {
+    mode: paletteType,
+    background: {
+      default: paletteType === 'light' ? '#eaeaea' : '#121212'
+    }
+  }
+})
+
+function handleThemeChange(){
+  setDDarkMode(!darkMode);
+}
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+      <Container>
+        <Catalog />
+      </Container>
+
+    </ThemeProvider>
+  );
+}
+
+export default App;

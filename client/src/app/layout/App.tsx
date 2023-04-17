691cfc0f311724575bb67825e698c2c83b4791f3
Lionel Ngobesing <ngobesinglionel@gmail.com>
2023-04-17 21:16:06 +0300

    Updating and finishing up the product catalog/menu

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -3,6 +3,7 @@ import { Container, CssBaseline, createTheme } from "@mui/material";
 import Header from "./header";
 import { ThemeProvider } from "@emotion/react";
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
 
 
@@ -29,7 +30,7 @@ function handleThemeChange(){
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalog />
+        <Outlet />
       </Container>
 
     </ThemeProvider>
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,6 +7,8 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './app/router/Routes';
 
 
 const root = ReactDOM.createRoot(
@@ -14,7 +16,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
